b6627ebf0ce083cbefa686af8aeafc378d9e8454
Immanuel Thoke <immanuel@matchwerk.de>
2020-01-31 00:41:27 +0100

    fix index.php

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     }
 
     error_reporting(E_ALL && E_WARNING && E_NOTICE);
-    ini_set('display_errors', 0);
+    ini_set('display_errors', 1);
     ini_set('log_errors', 1);
 
     require_once 'includes/common.inc.php';
 
     $requestParameters = RequestParametersParser::getRequestParameters($_POST, !empty($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : null);
-    _log($requestParameters);
 
     try
     {
@@ -26,9 +25,15 @@
 
         $dateTime = date('Y-m-d_H-i-s');
         $resultFilename = dirname(__FILE__) . "/result/quiz_result_{$dateTime}.txt";
-        @file_put_contents($resultFilename, $report);
+        
+        // some debugging
+        // $is_writable = is_writable('/var/www/html/result');
+        // echo $resultFilename . "is writable:" . $is_writable . "\n";
+        echo $report;
+        
+        // @file_put_contents($resultFilename, $report); the @ surpresses any error - use this in prod-env only
+        file_put_contents($resultFilename, $report);
 
-        echo "OK";
     }
     catch (Exception $e)
     {
@@ -43,5 +48,6 @@
         $event       = array('ts' => date('Y-m-d H:i:s'), 'request_parameters' => $requestParameters, 'ts_' => time());
         $logMessage  = json_encode($event);
         $logMessage .= ',' . PHP_EOL;
-        @file_put_contents($logFilename, $logMessage, FILE_APPEND);
+        // echo $logMessage; #for debugging purpose
+        file_put_contents($logFilename, $logMessage, FILE_APPEND);
     }
